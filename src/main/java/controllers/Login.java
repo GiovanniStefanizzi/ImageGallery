9eac3d777b6ded2a124e1f9c635bce60d2cff8b1
tomasostefanizzi <100636516+tomasostefanizzi@users.noreply.github.com>
2022-05-22 17:41:10 +0200

    login

---
+++ src/main/java/controllers/Login.java
@@ -0,0 +1,99 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.StringEscapeUtils;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import dao.*;
+import beans.*;
+
+import utility.ConnectionHandler;
+
+@WebServlet("/Login")
+
+public class Login extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	
+	public Login() {
+		super();
+	}
+	
+	Connection connection;
+	private TemplateEngine templateEngine;
+	
+	public void init() throws ServletException{
+		ServletContext context = getServletContext();
+		this.templateEngine = ConnectionHandler.instance.startTemplate(context);
+		connection = ConnectionHandler.instance.ConnectDb(context);
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String path = "/index.html";
+		ServletContext servletContext = getServletContext();
+		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+		templateEngine.process(path, ctx, response.getWriter());
+	}
+	
+	
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = null;
+		String password = null;
+		
+		ServletContext servletContext = getServletContext();
+		
+		
+		// Get and escape request parameters
+		username = StringEscapeUtils.escapeJava(request.getParameter("username"));
+		password = StringEscapeUtils.escapeJava(request.getParameter("password"));
+		if (username == null || password == null || username.isEmpty() || password.isEmpty()) {
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Missing parameters");
+			return;
+		}
+		
+		// query db to authenticate for user
+		UserDAO userDao = new UserDAO(connection);
+		User user = null;
+		try {
+			user = userDao.checkCredentials(username, password);
+		} catch (SQLException e) {
+			
+			response.sendError(HttpServletResponse.SC_BAD_GATEWAY, "Failure in database credential checking");
+			return;
+		}
+
+		// If the user exists, add info to the session and go to home page, otherwise
+		// return an error message
+		String path = "/index.html";
+		if (user == null) {
+			final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+			ctx.setVariable("errorMsg", "icorrect username or password");
+			templateEngine.process(path, ctx, response.getWriter());
+		} else {
+			request.getSession().setAttribute("user", user);
+			path = getServletContext().getContextPath() + "/Home";
+			response.sendRedirect(path);
+		}
+
+	}
+	
+
+}
+
--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,5 +1,38 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import beans.User;
+
 public class UserDAO {
+
+	private  Connection connection;
+	public UserDAO(Connection connection) {
+		this.connection = connection;
+	}
+	
+
+	public User checkCredentials(String username, String password) throws SQLException {
+		String query = "SELECT id, username, name, surname FROM users WHERE username = ? AND password = ?";
+		try (PreparedStatement pStatement = connection.prepareStatement(query);) {
+			pStatement.setString(1, username);
+			pStatement.setString(2, password);
+			try (ResultSet res = pStatement.executeQuery();) {
+				if(!res.isBeforeFirst()) // no result
+					return null;
+				else {
+					res.next();
+					User user = new User();
+					user.setId(res.getInt("id"));
+					user.setUserName(res.getString("username"));
+					user.setEmail(res.getString("email"));
+					return user;
+				}
+			}
+		}
+	}
 
 }
